1a7c68da32b3fe1843c3257421a7013587798702
OsdaGomez99 <osdalys.gomez99@gmail.com>
2022-08-14 22:11:13 -0400

    Implementacion de registro, login y logout con JWT

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('cedula')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('v1/consejo', ConsejoV1::class)
       ->only(['index','show', 'destroy'])
       ->middleware('auth:sanctum');
+/*
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
 
-Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
+*/
+
+    Route::group([
+
+        'middleware' => 'api',
+        'prefix' => 'auth'
+
+    ], function ($router) {
+
+        Route::post('register', 'App\Http\Controllers\Api\AuthController@register');
+        Route::post('login', 'App\Http\Controllers\Api\AuthController@login');
+        Route::post('logout', 'App\Http\Controllers\Api\AuthController@logout');
+        Route::post('refresh', 'App\Http\Controllers\Api\AuthController@refresh');
+        Route::post('me', 'App\Http\Controllers\Api\AuthController@me');
+
+    });
